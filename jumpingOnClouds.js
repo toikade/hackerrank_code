82124ff0e63a51c334368974394d8f8fb2d8b954
Aaron <toikadeaaron@gmail.com>
2023-09-27 13:16:07 +0100

    another solution for jumping on clouds

--- jumpingOnClouds.js
+++ jumpingOnClouds.js
@@ -1,29 +1,17 @@
 function jumpingOnClouds(c=[1, 1, 1, 0, 1, 1, 0, 0, 0, 0], k=3) {
-   let remainder=0;
-    let toComplete=0;
-    let energy=100;
-   let i=k;
-    let reduceEnergy=()=>c[i]==1?energy-=3:energy--;
-    do{
-        if(i+k>c.length-1){
-            reduceEnergy();
-            
-            remainder=c.length-1-i;
-            
-            toComplete=k-remainder;
-            
-            i=0+toComplete-1;
-            
-            //break;
-        }else{
-          
-        reduceEnergy();
-        
-        i+=k;  
-        }
-        
-        
-    }while(i!==0);
-   if(i==0) reduceEnergy();
-    return energy;
-}
+    let remainder=0;
+     let toComplete=0;
+     let energy=100;
+    let i=0;
+     let reduceEnergy=()=>c[i]==1?energy-=3:energy--;
+     do{
+        i=(i+k)%c.length;
+         //console.log(i);
+         //console.log('C',c[i]);
+         reduceEnergy();
+         //console.log('E',energy);
+     }while(i!==0);
+   // if(i==0) reduceEnergy();
+     return energy;
+ }
+ 
