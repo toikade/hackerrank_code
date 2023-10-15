2e60e8f1fda5ba874706672c316c04b092a4373a
Aaron <toikadeaaron@gmail.com>
2023-10-15 17:48:35 +0100

    7th trial

--- nonDivisibleSubset.js
+++ nonDivisibleSubset.js
@@ -15,31 +15,58 @@ function nonDivisibleSubset(k=3, s=[1,7,2,4]){
         countObj[rem]>=1?countObj[rem]++:countObj[rem]=1;//store the count of each remainder in countObj
     }
     
-   console.log('remArr', remainderArr);
-   console.log(countObj);
-            
-            let objArr=Object.keys(countObj);
-            let objLength=Object.keys(countObj).length;
-            for(let i=counter; i<=objLength-1; i++){
-                for(let j=i; j<=objLength-1; j++){
+   // console.log('remArr', remainderArr);
+   //console.log(countObj);
+      
+      
+      loopAnswer();  
+        function loopAnswer(){
+            //FOR EVERY ELEMENT IN theArr
+            for(let i=counter; i<=theArrLength-1; i++){
+               
+                //FOR EVERY ELEMENT IN theArr
+                for(let j=i; j<=theArrLength-1; j++){
+                    //continue if the both numbers are same (not needed)
+                     if(theArr[i]==theArr[j]) continue;
+                    //if the sum of 2 remainders in remainderArr==k then they cant be in same array
+                    if((remainderArr[i]+remainderArr[j])==k){
+                        //console.log(remainderArr[i],remainderArr[j]);
+                        //console.log(theArr[i], theArr[j]);
+                        if(!pairArr.length){ //if pairArr is still empty
+                           pairArr.push([remainderArr[i],remainderArr[j]]); 
+                        }
+                        //FOR EVERY ITEM IN pairArr
+                        for(let pair=0; pair<=pairArr.length-1; pair++){ 
+                            //if on last item in loop and (last item does not include remainderArr[i] and [j])=> found no match till now
+                            if((pair==pairArr.length-1)&&((!pairArr[pair].includes(remainderArr[i]))&&(!pairArr[pair].includes(remainderArr[j])))){
+                                //push the current remainderArr items into pairArr
+                                pairArr.push([remainderArr[i],remainderArr[j]]);
+                                //else if pairArr item is a match
+                            }else if((pairArr[pair].includes(remainderArr[i]))&&(pairArr[pair].includes(remainderArr[j]))){
+                                break;
+                            }else{
+                                continue;
+                            }
+                        }
+                       // console.log(pairArr);
+                    }//if
                     
-                    if(+objArr[i]+(+objArr[j])==k){
-                        console.log(objArr[i], objArr[j]);
-                        pairArr.push([+objArr[i],+objArr[j]]);
-                        console.log(pairArr);
-                    }
                 }
+                
+                counter++;
+              
             }
-            
             //FOR EVERY pair IN pairArr
             for(let pair of pairArr){
                 //if counts of 2 numbers are == return the second : count of pair[0] is smaller return pair[1] else pair[0]
                 numberToDelete=countObj[pair[0]]==countObj[pair[1]]?pair[1]:countObj[pair[0]]<countObj[pair[1]]?pair[0]:pair[1];
-                console.log(numberToDelete);
+               // console.log(numberToDelete);
                 //update remainderArr removing the value of numberToDelete
                 remainderArr=remainderArr.filter(value=>value!==numberToDelete);
                // console.log(remainderArr);
             }
+            
+        }
         
         return (k==0||k==1)?1:remainderArr.length;
 }
